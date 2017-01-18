b201ceda6891e3aa680a2e34d7b71a0877f19bc0
itgsod-hampus-bohman <hampus.bohman@elev.it-gymnasiet.se>
2017-01-18 13:59:57 +0100

    fixed tha fucking footer and added javascript shieet

---
+++ Javascript/Footer.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Hampus on 2017-01-18.
+ */
+function printFooter() {
+    document.write('<div class="footer"><p>Copyright &copy; 2017 Pontus Wiström, Hampus Bohman, Emil Hansen </p></div>');
+}
