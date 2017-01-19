40cc81dd4f26d39f3a189558aa7837694baf8225
Emil Hansen <emil.hansen@elev.it-gymnasiet.se>
2017-01-19 15:10:30 +0100

    läckert

--- Javascript/Footer.js
+++ Javascript/Footer.js
@@ -1,6 +1,1 @@
-/**
- * Created by Hampus on 2017-01-18.
- */
-function printFooter() {
-    document.write('<div class="footer"><p>Copyright &copy; 2017 Pontus Wiström, Hampus Bohman, Emil Hansen </p></div>');
-}
+
